1e4ea2d5014fc42b3787c32059cb1ebb59ac9d7c
Vitaly Gashkov <vitalygashkov@outlook.com>
2022-07-24 12:18:39 +0500

    feat(okko): download franchise episodes

--- src/providers/okko/okko.js
+++ src/providers/okko/okko.js
@@ -56,9 +56,49 @@ class Okko extends Provider {
         drmConfig,
       };
       configList.push(config);
+    } else if (type === ELEMENT_TYPE.FRANCHISE) {
+      const episodes = movieCard.element.children.items
+        .map((item) => item.element)
+        .filter(({ seqNo }) => !this.#args.episodes || this.#args.episodes.includes(seqNo));
+      const configs = await this.#getEpisodeConfigs(episodes);
+      configList.push(...configs);
+    } else if (type === ELEMENT_TYPE.TV) {
+      const seasons = movieCard.element.children.items.map((item) => item.element);
     }
 
     return configList;
+  }
+
+  async #getEpisodeConfigs(episodes) {
+    const configs = [];
+    const elements = episodes.map(({ id, type }) => ({ id, type }));
+    for (const element of elements) {
+      const playbackInfo = await this.#api.preparePlayback([element]);
+      const item = playbackInfo.elements.items[0];
+      const assetItems = item.assets.items;
+      const assets = assetItems.filter(({ media }) => !media.drmType || media.drmType === 'CENC');
+      const config = {
+        provider: PROVIDER_TAG,
+        manifestUrl: assets[0].url,
+        drmConfig: {
+          server: item.licenses.items[0].licenseServerUrls.CENC_WIDEVINE,
+          http2: true,
+        },
+      };
+      if (item.type === ELEMENT_TYPE.MOVIE) config.movie = { title: item.originalName };
+      if (item.type === ELEMENT_TYPE.EPISODE) {
+        const showTitle = item.originalName.replace(`. Серия ${item.seqNo}`, '');
+        const episodeTitle = episodes.find((ep) => ep.id === item.id)?.title;
+        config.show = { title: showTitle };
+        config.season = { number: null };
+        config.episode = {
+          number: item.seqNo,
+          title: showTitle === episodeTitle ? null : episodeTitle,
+        };
+      }
+      configs.push(config);
+    }
+    return configs;
   }
 }
 
