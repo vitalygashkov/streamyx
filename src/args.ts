28257a7f709a3b78a05cee6defe0cb02ce0bf40a
Vitaly Gashkov <vitalygashkov@vk.com>
2023-07-09 19:23:28 +0500

    refactor(args): use built-in node parser

--- src/args.ts
+++ src/args.ts
@@ -1,278 +1,160 @@
 import { platform, arch, version } from 'node:process';
+import { parseArgs, ParseArgsConfig } from 'node:util';
+import packageInfo from '../package.json';
+import { parseArrayFromString, parseHeadersFromString, parseNumberRange } from './utils';
 
-const formatArgLabel = (rawLabel: string) => {
-  const labelParts = rawLabel
-    .replace(/--/g, '-')
-    .split('-')
-    .filter((i) => !!i);
-  return labelParts
-    .map((word, index) => (index > 0 ? word.charAt(0).toUpperCase() + word.slice(1) : word))
-    .join('');
-};
-
-class Args {
-  #argv;
-  #data: {
-    arguments: { name: string; description: string }[];
-    options: { flags: string; description: string; defaultValue?: string | number | boolean }[];
-    name?: string;
-    description?: string;
-    version?: string;
-  };
-  parsed;
-  _?: any[];
-
-  constructor(argv = process.argv.slice(2)) {
-    this.parsed = false;
-    this.#argv = argv;
-    this.#data = { arguments: [], options: [] };
-  }
-
-  setName(name: string) {
-    this.#data.name = name;
-    return this;
-  }
-
-  setDescription(description: string) {
-    this.#data.description = description;
-    return this;
-  }
-
-  setVersion(version: string) {
-    this.#data.version = version;
-    return this;
-  }
-
-  setArgument(name: string, description: string) {
-    this.#data.arguments.push({ name, description });
-    return this;
-  }
-
-  setOption(flags: string, description: string, defaultValue?: string | number | boolean) {
-    this.#data.options.push({ flags, description, defaultValue });
-    return this;
-  }
-
-  parse(indexOfNamelessArg = -1) {
-    if (!this.#argv.length) return this;
-    const isNamelessArgAtBegin = this.#argv[0][0] !== '-';
-    if (!isNamelessArgAtBegin) {
-      this._ = [this.#argv.at(indexOfNamelessArg)];
-      this.#argv.splice(indexOfNamelessArg, 1);
-    }
-
-    const parsedArgs: Record<string, string | number | boolean | string[]> = {};
-    const argv = this.#argv;
-
-    for (let i = 0; i < argv.length; i++) {
-      const argument = argv[i];
-      const previousArgument = argv[i - 1];
-      const nextArgument = argv[i + 1];
-      const isLabel = argv[i][0] === '-';
-
-      if (isLabel) {
-        const isLabelHasValue = nextArgument?.[0] !== undefined && nextArgument?.[0] !== '-';
-        const label = formatArgLabel(argument);
-        const value = isLabelHasValue ? nextArgument : true;
-        parsedArgs[label] = value;
-      } else {
-        const isValueHasLabel = previousArgument?.[0] === '-';
-        if (!isValueHasLabel)
-          parsedArgs._ = Array.isArray(parsedArgs._) ? [...parsedArgs._, argument] : [argument];
-      }
-    }
-
-    Object.keys(parsedArgs).forEach((key) => ((this as any)[key] = parsedArgs[key]));
-    this.parsed = true;
-
-    const hasArgs = parsedArgs ? Object.keys(parsedArgs).length > 0 : false;
-    if (!hasArgs || 'h' in this || 'help' in this) this.outputHelp();
-    if ('v' in this || 'version' in this) (this as Args).outputVersion();
-
-    return this;
-  }
-
-  outputHelp() {
-    this.outputDescription();
-    this.outputVersion();
-    this.outputUsage();
-    this.outputArguments();
-    this.outputOptions();
-    process.exit(1);
-  }
-
-  outputDescription() {
-    if (this.#data.description) console.log(`${this.#data.name}: ${this.#data.description}\n`);
-  }
-
-  outputUsage() {
-    console.log(`\x1b[1mUSAGE\x1b[0m`);
-    let message = `  $ ${this.#data.name} `;
-    if (this.#data.options.length) message += `[OPTIONS] `;
-    if (this.#data.arguments.length) message += this.#data.arguments.map((a) => a.name).join(' ');
-    console.log(`${message}\n`);
-  }
-
-  outputArguments() {
-    if (!this.#data.arguments.length) return;
-    console.log(`\x1b[1mARGUMENTS\x1b[0m`);
-    for (const argument of this.#data.arguments) {
-      console.log(`  ${argument.name}  ${argument.description}\n`);
-    }
-  }
-
-  outputOptions() {
-    if (this.#data.options) {
-      console.log(`\x1b[1mOPTIONS\x1b[0m`);
-      this.#data.options.forEach((option) =>
-        console.log(`  ${option.flags.padEnd(20)}  ${option.description}`)
-      );
-    }
-  }
-
-  outputVersion() {
-    console.log(`\x1b[1mVERSION\x1b[0m`);
-    console.log(`  ${this.#data.name}/${this.#data.version} ${platform}-${arch} node-${version}\n`);
-  }
+interface Option {
+  type: 'string' | 'boolean';
+  multiple?: boolean | undefined;
+  short?: string | undefined;
+  default?: string | boolean | string[] | boolean[] | undefined;
+  description?: string;
 }
 
-const pushOrSet = (obj: any, key: any, value: any) => {
-  if (Array.isArray(obj[key])) obj[key].push(value);
-  else obj[key] = value;
+interface ParseArgsConfigWithDescriptions extends ParseArgsConfig {
+  options: Record<string, Option>;
+}
+
+const positionals = {
+  URL: {
+    description:
+      'Content URL like movie, season, series, etc. (use quotes "..." if URL includes specific symbols, like &)',
+  },
 };
 
-const parseFlag = (parsed: any, args: any, currentIndex: any, opt: any, flag: any) => {
-  if (opt.type === Boolean) {
-    pushOrSet(parsed, opt.name, true);
-  } else {
-    const nextValue = args[++currentIndex];
-    if (nextValue === undefined) {
-      if (opt.optionalValue) {
-        pushOrSet(parsed, opt.name, true);
-      } else {
-        throw new Error(`missing value for ${flag}`);
-      }
-    } else {
-      pushOrSet(parsed, opt.name, opt.type(nextValue));
-    }
-  }
-  return currentIndex;
+const config: ParseArgsConfigWithDescriptions = {
+  allowPositionals: true,
+  options: {
+    'video-quality': {
+      short: 'q',
+      type: 'string',
+      description: 'sets video quality; example: 1080p',
+    },
+    'audio-quality': { short: 'a', type: 'string', description: 'sets audio quality' },
+    episodes: {
+      short: 'e',
+      type: 'string',
+      description: 'sets episode numbers; example: 1,4-10,16,17',
+    },
+    seasons: { short: 's', type: 'string', description: 'sets season numbers; example: 1,4,5-8' },
+    template: {
+      short: 't',
+      type: 'string',
+      description: 'filename template; example: "{title} - S{s}E{e} [{quality} {translation}]"',
+    },
+    'movie-template': {
+      type: 'string',
+      description: 'movie filename template',
+      default: '{title}.{audioType}.{quality}.{provider}.{format}.{codec}',
+    },
+    'episode-template': {
+      type: 'string',
+      description: 'episode filename template',
+      default: '{show}.S{s}E{e}.{title}.{audioType}.{quality}.{provider}.{format}.{codec}',
+    },
+    connections: {
+      short: 'c',
+      type: 'string',
+      default: '16',
+      description: 'number of parallel http connections per download (default: 16)',
+    },
+    hdr: { type: 'boolean', description: 'select high dynamic range video track if available' },
+    '3d': { type: 'boolean', description: 'select 3D video track if available' },
+    hardsub: { type: 'boolean', description: 'download hardsubbed video if available' },
+    'subs-lang': {
+      type: 'string',
+      description: 'download subtitles by language tag (en, ru, etc.)',
+    },
+    'audio-lang': { type: 'string', description: 'download audio by language tag (en, ru, etc.)' },
+    'skip-subs': { type: 'boolean', description: 'do not download subtitles' },
+    'skip-audio': { type: 'boolean', description: 'do not download audio' },
+    'skip-video': { type: 'boolean', description: 'do not download video' },
+    'skip-mux': {
+      type: 'boolean',
+      description: 'do not mux tracks like video, audio and subtitles',
+    },
+    'trim-begin': { type: 'string', description: 'trim video at the beginning; example: 00:00:06' },
+    'trim-end': { type: 'string', description: 'trim video at the end; example: 00:01:30' },
+    pssh: {
+      type: 'string',
+      description:
+        'widevine PSSH from MPD manifest (print decryption keys only; url argument should be license url)',
+    },
+    headers: {
+      type: 'string',
+      description:
+        'headers for license request (--pssh argument required; example: "Content-Type:application/json|Cookie:SessionID=yIz9I")',
+    },
+    debug: { short: 'd', type: 'boolean', description: 'debug mode', default: false },
+    version: { short: 'v', type: 'boolean', description: 'print version', default: false },
+    help: { short: 'h', type: 'boolean', description: 'print help', default: false },
+  },
 };
 
-const parsePositional = (parsed: any, args: any, currentIndex: any, opt: any) => {
-  if (!opt.multiple) {
-    parsed[opt.name] = opt.type(args[currentIndex]);
-    return currentIndex;
-  }
-
-  const values = [opt.type(args[currentIndex])];
-
-  for (let i = currentIndex + 1; i < args.length; i++) {
-    const value = args[i];
-    if (value && value[0] === '-' && opt.multiple !== 'include-flags') {
-      break;
-    } else if (value) {
-      currentIndex += 1;
-      values.push(opt.type(value));
-    }
-  }
-
-  parsed[opt.name] = values;
-  return currentIndex;
+export const getProcessedArgs = () => {
+  const args = parseArgs(config);
+  const { values, positionals } = args;
+  return {
+    urls: positionals,
+    videoHeight: parseInt(String(values['video-quality'] || '').replaceAll('p', '')),
+    audioQuality: values['audio-quality'],
+    episodes: parseNumberRange(String(values.episodes || '')),
+    seasons: parseNumberRange(String(values.seasons || '')),
+    movieTemplate:
+      values['movie-template'] || '{title}.{audioType}.{quality}.{provider}.{format}.{codec}',
+    episodeTemplate:
+      values['episode-template'] ||
+      '{show}.S{s}E{e}.{title}.{audioType}.{quality}.{provider}.{format}.{codec}',
+    connections: parseInt(String(values.connections)),
+    subtitleLanguages: parseArrayFromString(String(values['subs-lang'] || '')),
+    audioLanguages: parseArrayFromString(String(values['audio-lang'] || '')),
+    pssh: String(values.pssh || ''),
+    headers: parseHeadersFromString(String(values.headers || '')),
+    skipSubtitles: values['skip-subs'],
+    debug: values.debug,
+    version: values.version,
+    help: values.help,
+  };
 };
 
-// -abc 1 => -a -b -c 1
-const splitShortFlags = (arg: any) => {
-  if (/^-[a-zA-Z]/.test(arg)) {
-    return arg
-      .slice(1)
-      .split('')
-      .map((flag: any) => `-${flag}`);
-  }
-  return [arg];
+export const printVersion = () => {
+  console.log(`\x1b[1mVERSION\x1b[0m`);
+  console.log(`  ${packageInfo.name}/${packageInfo.version} ${platform}-${arch} node-${version}\n`);
 };
 
-const parse = (args: any, options: any) => {
-  const parsed: Record<string, string | string[] | boolean | number> = { _: [] };
-  let stopped = false;
+const printDescription = () => console.log(`${packageInfo.name}: ${packageInfo.description}\n`);
 
-  // when `option.when` returns false, they will be skipped
-  const skippedPositionalArgs = new Set();
-
-  args = args.reduce((res: any, arg: any) => {
-    if (arg[0] === '-') {
-      const equalSignIndex = arg.indexOf('=');
-      if (equalSignIndex > 0) {
-        res.push(...splitShortFlags(arg.slice(0, equalSignIndex)), arg.slice(equalSignIndex + 1));
-      } else {
-        res.push(...splitShortFlags(arg));
-      }
-    } else {
-      res.push(arg);
-    }
-    return res;
-  }, []);
-
-  for (let i = 0; i < args.length; i++) {
-    const flag = args[i];
-    const flagName = flag.replace(/^-{1,2}/, '');
-    if (stopped) {
-      (parsed._ as string[]).push(flag);
-      continue;
-    }
-    if (flag.startsWith('-')) {
-      const opt = options.find(
-        (o: any) =>
-          !o.positional &&
-          (o.name === flagName || o.flags?.includes(flagName)) &&
-          (!o.when || o.when(parsed))
-      );
-      if (opt) {
-        if (opt.multiple) {
-          parsed[opt.name] = parsed[opt.name] || [];
-        }
-        i = parseFlag(parsed, args, i, opt, flag);
-        if (opt.stop) {
-          stopped = true;
-        }
-      } else {
-        throw new Error(`unknown flag: ${flag}`);
-      }
-    } else {
-      const opt = options.find((o: any) => {
-        return (
-          o.positional &&
-          parsed[o.name] === undefined &&
-          (!o.when || o.when(parsed) || !skippedPositionalArgs.add(o.name))
-        );
-      });
-      if (opt) {
-        i = parsePositional(parsed, args, i, opt);
-        if (opt.stop) {
-          stopped = true;
-        }
-      } else {
-        throw new Error(`unknown positional argument: ${flag}`);
-      }
-    }
-  }
-
-  // check required positional arguments
-  for (const opt of options) {
-    if (
-      opt.positional &&
-      !opt.optionalValue &&
-      parsed[opt.name] === undefined &&
-      !skippedPositionalArgs.has(opt.name)
-    ) {
-      if (opt.when && !opt.when(parsed)) {
-        continue;
-      }
-      throw new Error(`missing positional argument: ${opt.name}`);
-    }
-  }
-
-  return parsed;
+const printUsage = (options: any, args: { name: string }[]) => {
+  console.log(`\x1b[1mUSAGE\x1b[0m`);
+  let message = `  $ ${packageInfo.name} `;
+  if (Object.keys(options).length) message += `[OPTIONS] `;
+  if (args.length) message += args.map((a) => a.name).join(' ');
+  console.log(`${message}\n`);
 };
 
-export { Args, parse };
+const printArguments = (data: Record<string, { description: string }> = {}) => {
+  if (!Object.keys(data).length) return;
+  console.log(`\x1b[1mARGUMENTS\x1b[0m`);
+  for (const [name, argument] of Object.entries(data))
+    console.log(`  ${name}  ${argument.description}\n`);
+};
+
+const printOptions = (options?: Record<string, Option>) => {
+  if (options) {
+    console.log(`\x1b[1mOPTIONS\x1b[0m`);
+    for (const [name, option] of Object.entries(options)) {
+      const flags = [option.short ? '-' + option.short : '', '--' + name]
+        .filter(Boolean)
+        .join(', ');
+      console.log(`  ${flags.padEnd(20)} ${option.description}`);
+    }
+  }
+};
+
+export const printHelp = () => {
+  printDescription();
+  printVersion();
+  printUsage(config.options, []);
+  printArguments(positionals);
+  printOptions(config.options);
+};
--- streamyx.ts
+++ streamyx.ts
@@ -1,87 +1,11 @@
 process.title = 'streamyx';
 
-import packageInfo from './package.json';
-import { Args } from './src/args';
+import { getProcessedArgs, printHelp, printVersion } from './src/args';
 import { logger } from './src/logger';
-import {
-  parseArrayFromString,
-  parseHeadersFromString,
-  parseMainDomain,
-  parseNumberRange,
-  validateUrl,
-} from './src/utils';
+import { parseMainDomain, validateUrl } from './src/utils';
 import { printDecryptionKeys } from './src/drm';
 import { createProvider } from './src/providers';
 import { Downloader } from './src/downloader';
-
-const args = new Args()
-  .setName(packageInfo.name)
-  .setVersion(packageInfo.version)
-  .setDescription(packageInfo.description)
-  .setArgument(
-    'URL',
-    'Content URL like movie, season, series, etc. (use quotes "..." if URL includes specific symbols, like &)'
-  )
-  .setOption('-q, --video-quality', 'sets video quality; example: 1080p')
-  .setOption('-a, --audio-quality', 'sets audio quality')
-  .setOption('-e, --episodes', 'sets episode numbers; example: 1,4-10,16,17')
-  .setOption('-s, --seasons', 'sets season numbers; example: 1,4,5-8')
-  // .setOption('-f, --force', 'overwrite output files without asking')
-  .setOption(
-    '-t, --template',
-    'filename template; example: "{title} - S{s}E{e} [{quality} {translation}]"'
-  )
-  .setOption('--movie-template', 'movie filename template')
-  .setOption(
-    '--episode-template',
-    'episode filename template; example: "{show}.S{s}E{e}.{title}.{quality}.{provider}.{format}.{codec}"'
-  )
-  // .setOption('-p, --proxy', 'set http(s)/socks proxy (WHATWG URL standard)')
-  .setOption('-c, --connections', 'number of parallel http connections per download (default: 16)')
-  .setOption('--hdr', 'select high dynamic range video track if available')
-  .setOption('--3d', 'select 3D video track if available')
-  .setOption('--hardsub', 'download hardsubbed video if available')
-  .setOption('--subs-lang', 'download subtitles by language tag (en, ru, etc.)')
-  .setOption('--audio-lang', 'download audio by language tag (en, ru, etc.)')
-  .setOption('--skip-subs', 'do not download subtitles')
-  .setOption('--skip-audio', 'do not download audio')
-  .setOption('--skip-video', 'do not download video')
-  .setOption('--skip-mux', 'do not mux tracks like video, audio and subtitles')
-  .setOption('--trim-begin', 'trim video at the beginning; example: 00:00:06')
-  .setOption('--trim-end', 'trim video at the end; example: 00:01:30')
-  .setOption(
-    '--pssh',
-    'Widevine PSSH from MPD manifest (print decryption keys only; url argument should be license url)'
-  )
-  .setOption(
-    '--headers',
-    'Headers for license request (--pssh argument required; example: "Content-Type:application/json|Cookie:SessionID=yIz9I")'
-  )
-  .setOption('-d, --debug', 'debug mode')
-  .setOption('-v, --version', 'output version')
-  .setOption('-h, --help', 'output help')
-  .parse();
-
-const parseArgs = (args: any) => ({
-  ...args,
-  urls: args._,
-  videoHeight: parseInt(String(args.q || args.videoQuality || '').replaceAll('p', '')),
-  audioQuality: args.a || args.audioQuality,
-  episodes: parseNumberRange(args.e || args.episodes),
-  seasons: parseNumberRange(args.s || args.seasons),
-  force: args.f || args.force,
-  movieTemplate: args.movieTemplate || '{title}.{audioType}.{quality}.{provider}.{format}.{codec}',
-  episodeTemplate:
-    args.episodeTemplate ||
-    '{show}.S{s}E{e}.{title}.{audioType}.{quality}.{provider}.{format}.{codec}',
-  proxy: args.p || args.proxy,
-  connections: parseInt(String(args.c || args.connections)) || args.c || args.connections || 16,
-  subtitleLanguages: parseArrayFromString(args.subsLang),
-  audioLanguages: parseArrayFromString(args.audioLang),
-  headers: parseHeadersFromString(args.headers),
-  skipSubtitles: args.skipSubs,
-  debug: args.d ?? args.debug,
-});
 
 const streamyx: any = {
   logger,
@@ -109,17 +33,19 @@ const loadProvider = async (name: string, args: any) => {
 };
 
 const loadProviders = async () => {
-  const parsedArgs: Record<string, any> = parseArgs(args);
-  streamyx.logger.setLogLevel(parsedArgs.debug ? 'debug' : 'info');
-  streamyx.downloader = new Downloader(parseArgs);
-  const urls: string[] = parsedArgs.urls ?? [''];
+  const args = getProcessedArgs();
+  if (args.version) printVersion();
+  if (args.help) printHelp();
+  streamyx.logger.setLogLevel(args.debug ? 'debug' : 'info');
+  streamyx.downloader = new Downloader(args);
+  const urls: string[] = args.urls ?? [''];
   for (const url of urls) {
-    if (parsedArgs.pssh) {
-      await printDecryptionKeys(url, parsedArgs.pssh, parsedArgs.headers);
+    if (args.pssh) {
+      await printDecryptionKeys(url, args.pssh, args.headers);
       break;
     }
     const domain = parseMainDomain(await validateUrl(url));
-    if (domain) await loadProvider(domain, parsedArgs);
+    if (domain) await loadProvider(domain, args);
   }
 };
 
