8c8d81926e6902f7a583e4ac317d6d0a33510083
Vitaly Gashkov <vitalygashkov@vk.com>
2024-07-07 13:02:13 +0500

    feat(core): add method to create streamyx instance

--- packages/core/lib/main.ts
+++ packages/core/lib/main.ts
@@ -1,3 +1,11 @@
+import { StreamyxInstance } from './plugin';
+import { http } from './http';
+import { default as fs } from './fs';
+import { logger as log } from './logger';
+import { prompt } from './prompt';
+
+export const create = (): StreamyxInstance => ({ log, http, prompt, fs });
+
 export { default as fs } from './fs';
 export * from './http';
 export * from './settings';
