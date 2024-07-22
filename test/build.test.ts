63a8962019c166275684ec87558e19bbf40d462d
Vitaly <vitalygashkov@vk.com>
2024-07-22 14:43:56 +0500

    fix: extend timeout for vitest test

--- test/build.test.ts
+++ test/build.test.ts
@@ -16,40 +16,44 @@ const getBuildDirPrefix = () => {
   }
 };
 
-test('test executable run with Okko', async () => {
-  const buildDir = path.join(__dirname, '../build');
-  const buildDirPrefix = getBuildDirPrefix();
-  const executableDir = fs.readdirSync(buildDir).find((file) => file.startsWith(buildDirPrefix));
-  expect(executableDir).toBeDefined();
-  console.log(`Executable folder: ${executableDir}`);
-  const executableSuffix = process.platform.startsWith('win') ? '.exe' : '';
-  const executablePath = path.join(buildDir, executableDir!, 'streamyx' + executableSuffix);
-  console.log(`Executable path: ${executablePath}`);
-  const streamyx = spawn(executablePath, [
-    'https://okko.tv/serial/hozukis-coolheadedness/season/1/episode/13',
-    '--debug',
-  ]);
-  const dataLines: string[] = [];
-  streamyx.stdout.on('data', (data: Buffer) => {
-    if (!data) return;
-    const dataStr = data.toString();
-    dataLines.push(dataStr);
-    console.log(dataStr);
-  });
-  const errorLines: string[] = [];
-  streamyx.stderr.on('error', (error: Buffer) => {
-    if (!error) return;
-    const errorStr = error.toString();
-    errorLines.push(errorStr);
-    console.log(errorStr);
-  });
-  const searchTarget = 'Open page https://okko.tv/tv and enter';
-  const result = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(dataLines.find((line) => line.includes(searchTarget)));
-    }, 10_000);
-  });
-  expect(result).toBeDefined();
-  expect(result).toContain(searchTarget);
-  streamyx.kill();
-});
+test(
+  'test executable run with Okko',
+  async () => {
+    const buildDir = path.join(__dirname, '../build');
+    const buildDirPrefix = getBuildDirPrefix();
+    const executableDir = fs.readdirSync(buildDir).find((file) => file.startsWith(buildDirPrefix));
+    expect(executableDir).toBeDefined();
+    console.log(`Executable folder: ${executableDir}`);
+    const executableSuffix = process.platform.startsWith('win') ? '.exe' : '';
+    const executablePath = path.join(buildDir, executableDir!, 'streamyx' + executableSuffix);
+    console.log(`Executable path: ${executablePath}`);
+    const streamyx = spawn(executablePath, [
+      'https://okko.tv/serial/hozukis-coolheadedness/season/1/episode/13',
+      '--debug',
+    ]);
+    const dataLines: string[] = [];
+    streamyx.stdout.on('data', (data: Buffer) => {
+      if (!data) return;
+      const dataStr = data.toString();
+      dataLines.push(dataStr);
+      console.log(dataStr);
+    });
+    const errorLines: string[] = [];
+    streamyx.stderr.on('error', (error: Buffer) => {
+      if (!error) return;
+      const errorStr = error.toString();
+      errorLines.push(errorStr);
+      console.log(errorStr);
+    });
+    const searchTarget = 'Open page https://okko.tv/tv and enter';
+    const result = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(dataLines.find((line) => line.includes(searchTarget)));
+      }, 10_000);
+    });
+    expect(result).toBeDefined();
+    expect(result).toContain(searchTarget);
+    streamyx.kill();
+  },
+  { timeout: 20_000 }
+);
