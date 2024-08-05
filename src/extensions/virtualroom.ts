d71f388eb160bccee427b808ffe4c0b0c69a3a4e
Vitaly Gashkov <vitalygashkov@vk.com>
2024-08-05 23:22:48 +0500

    refactor: remove redundant types

--- src/extensions/virtualroom.ts
+++ src/extensions/virtualroom.ts
@@ -1,30 +1,28 @@
-import type { PluginInstance, StreamyxCore } from '@streamyx/core';
+import type { StreamyxCore } from '@streamyx/core';
 
-export const virtualroom =
-  () =>
-  (core: StreamyxCore): PluginInstance => ({
-    name: 'virtualroom',
-    fetchMediaInfo: async (url) => {
-      const recordId = new URL(url).searchParams.get('recordId');
+export const virtualroom = () => (core: StreamyxCore) => ({
+  name: 'virtualroom',
+  fetchMediaInfo: async (url: string) => {
+    const recordId = new URL(url).searchParams.get('recordId');
 
-      const infoUrl = `https://mv1.virtualroom.ru/vr/player/records/${recordId}/info`;
-      const infoResponse = await core.http.fetch(infoUrl);
-      const info = await infoResponse.json();
+    const infoUrl = `https://mv1.virtualroom.ru/vr/player/records/${recordId}/info`;
+    const infoResponse = await core.http.fetch(infoUrl);
+    const info = await infoResponse.json();
 
-      const eventsUrl = `https://mv1.virtualroom.ru/vr/player/records/${recordId}/events`;
-      const eventsResponse = await core.http.fetch(eventsUrl);
-      const events = await eventsResponse.json();
+    const eventsUrl = `https://mv1.virtualroom.ru/vr/player/records/${recordId}/events`;
+    const eventsResponse = await core.http.fetch(eventsUrl);
+    const events = await eventsResponse.json();
 
-      const mediaInfoList = [];
-      for (const translation of events.data.translations) {
-        const title = `${info.data.roomParameters.name} ${translation.type} ${translation.source} ${translation.start}`;
-        const mediaInfo = {
-          url: translation.url,
-          provider: 'VRM',
-          movie: { title },
-        };
-        mediaInfoList.push(mediaInfo);
-      }
-      return mediaInfoList;
-    },
-  });
+    const mediaInfoList = [];
+    for (const translation of events.data.translations) {
+      const title = `${info.data.roomParameters.name} ${translation.type} ${translation.source} ${translation.start}`;
+      const mediaInfo = {
+        url: translation.url,
+        provider: 'VRM',
+        movie: { title },
+      };
+      mediaInfoList.push(mediaInfo);
+    }
+    return mediaInfoList;
+  },
+});
--- src/extensions/vk.ts
+++ src/extensions/vk.ts
@@ -1,48 +1,46 @@
-import type { MediaInfo, StreamyxCore, PluginInstance } from '@streamyx/core';
+import type { MediaInfo, StreamyxCore, RunArgs } from '@streamyx/core';
 
-export const vk =
-  () =>
-  (core: StreamyxCore): PluginInstance => ({
-    name: 'vk',
-    fetchMediaInfo: async (url, args) => {
-      const html = await core.http
-        .fetch(url)
-        .then((r) => r.arrayBuffer())
-        .then((r) => new TextDecoder('windows-1251').decode(r))
-        .catch(() => '');
-      if (!html) core.log.error('Could not fetch video info');
+export const vk = () => (core: StreamyxCore) => ({
+  name: 'vk',
+  fetchMediaInfo: async (url: string, args: RunArgs) => {
+    const html = await core.http
+      .fetch(url)
+      .then((r) => r.arrayBuffer())
+      .then((r) => new TextDecoder('windows-1251').decode(r))
+      .catch(() => '');
+    if (!html) core.log.error('Could not fetch video info');
 
-      const js = JSON.parse('{"lang":' + html.split(`{"lang":`)[1].split(']);')[0]);
+    const js = JSON.parse('{"lang":' + html.split(`{"lang":`)[1].split(']);')[0]);
 
-      if (Number(js.mvData.is_active_live) !== 0) {
-        core.log.error('Live videos are not supported');
-        return [];
+    if (Number(js.mvData.is_active_live) !== 0) {
+      core.log.error('Live videos are not supported');
+      return [];
+    }
+
+    const selectedQuality = args.videoQuality?.replace('p', '').trim();
+    const resolutions = ['2160', '1440', '1080', '720', '480', '360', '240'];
+    let quality = selectedQuality || '2160';
+    for (const i in resolutions) {
+      if (js.player.params[0][`url${resolutions[i]}`]) {
+        quality = resolutions[i];
+        break;
       }
+    }
+    if (selectedQuality && Number(quality) > Number(selectedQuality)) quality = selectedQuality;
 
-      const selectedQuality = args.videoQuality?.replace('p', '').trim();
-      const resolutions = ['2160', '1440', '1080', '720', '480', '360', '240'];
-      let quality = selectedQuality || '2160';
-      for (const i in resolutions) {
-        if (js.player.params[0][`url${resolutions[i]}`]) {
-          quality = resolutions[i];
-          break;
-        }
-      }
-      if (selectedQuality && Number(quality) > Number(selectedQuality)) quality = selectedQuality;
-
-      const mediaInfo: MediaInfo[] = [];
-      const mediaUrl = js.player.params[0][`url${quality}`];
-      if (!mediaUrl) {
-        core.log.error('Could not find video URL');
-        return mediaInfo;
-      }
-      const title = js.player.params[0].md_title.trim();
-      const author = js.player.params[0].md_author.trim();
-      mediaInfo.push({
-        url: mediaUrl,
-        provider: 'VK',
-        movie: { title: `${title} ${author}` },
-      });
-      return mediaInfo;
-    },
-  });
+    const mediaInfoList: MediaInfo[] = [];
+    const mediaUrl = js.player.params[0][`url${quality}`];
+    if (!mediaUrl) {
+      core.log.error('Could not find video URL');
+      return mediaInfoList;
+    }
+    const title = js.player.params[0].md_title.trim();
+    const author = js.player.params[0].md_author.trim();
+    mediaInfoList.push({
+      url: mediaUrl,
+      provider: 'VK',
+      movie: { title: `${title} ${author}` },
+    });
+    return mediaInfoList;
+  },
+});
