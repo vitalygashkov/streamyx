0de4883b4bd7db241a088153315ce27ba37651ad
Vitaly Gashkov <vitalygashkov@vk.com>
2024-08-10 13:55:53 +0500

    feat: add soundcloud support, add core utils

--- src/core/lib/main.ts
+++ src/core/lib/main.ts
@@ -4,6 +4,7 @@ import { Http } from './http';
 import { default as fs } from './fs';
 import { logger as log } from './logger';
 import { prompt } from './prompt';
+import { sanitizeString, execUrlPatterns } from './utils';
 
 export const create = (name: string): StreamyxCore => ({
   log,
@@ -11,6 +12,7 @@ export const create = (name: string): StreamyxCore => ({
   prompt,
   fs,
   store: createStore(name),
+  utils: { sanitizeString, execUrlPatterns },
 });
 
 export { default as fs } from './fs';
