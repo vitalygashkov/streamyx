a7ed1dc54e73c84d2cfa5c85e499e948e4a2cba6
Vitaly Gashkov <vitalygashkov@vk.com>
2024-08-11 12:11:24 +0500

    feat(weibo): live photo downloads, multiple video in post support

--- src/extensions/weibo.ts
+++ src/extensions/weibo.ts
@@ -4,38 +4,37 @@ export const weibo = () => (core: StreamyxCore) => {
   return {
     name: 'weibo',
     fetchMediaInfo: async (url: string, args: RunArgs) => {
-      const patterns = ['/:userId/:blogId', '/detail/:mid'];
+      const patterns = ['/:userId(\\d+)/:bid', '/detail/:mid'];
       const baseUrls = ['https://weibo.com/', 'https://m.weibo.cn'];
       const result = core.utils.execUrlPatterns(url, patterns, baseUrls);
       const params = result.pathname;
-
       const mediaInfoList: MediaInfo[] = [];
-      if (params.blogId) {
-        const response = await fetch(`https://m.weibo.cn/statuses/show?id=${params.blogId}`, {
+      const id = params.bid || params.mid;
+      if (id) {
+        const response = await fetch(`https://m.weibo.cn/statuses/show?id=${id}`, {
           credentials: 'include',
-          referrer: `https://m.weibo.cn/status/${params.blogId}?jumpfrom=weibocom`,
+          referrer: `https://m.weibo.cn/status/${params.bid}?jumpfrom=weibocom`,
           method: 'GET',
           mode: 'cors',
         });
         const json = await response.json();
-        const pageInfo = json.data.page_info;
-        if (pageInfo) {
-          const pageInfoUrl = Object.values(pageInfo.urls)?.[0] as string | undefined;
-          const mediaInfoUrl =
-            pageInfo.media_info?.stream_url_hd || pageInfo.media_info?.stream_url;
+        const pics = json.data.pics;
+        const info = json.data.page_info;
+        if (pics) {
+          const videos = pics.filter(
+            (pic: any) => pic.type === 'video' || pic.type === 'livephoto'
+          );
+          for (const video of videos) {
+            mediaInfoList.push({ url: video.videoSrc, movie: { title: video.pid } });
+          }
+        } else if (info.type === 'video') {
+          const pageInfoUrl = Object.values(info.urls)?.[0] as string | undefined;
+          const mediaInfoUrl = info.media_info?.stream_url_hd || info.media_info?.stream_url;
           const url = pageInfoUrl || mediaInfoUrl;
-          const title = pageInfo.title || pageInfo.page_title;
+          const title = info.title || info.page_title;
           mediaInfoList.push({ url, movie: { title } });
         }
-      } else {
-        const response = await core.http.fetch(url);
-        const text = await response.text();
-        const pageInfoText = text.split('page_info":')[1]?.split('"bid"')[0].trim().slice(0, -1);
-        const pageInfo = JSON.parse(pageInfoText);
-        const mediaUrl = Object.values(pageInfo.urls)?.[0] as string | undefined;
-        if (mediaUrl) mediaInfoList.push({ url: mediaUrl, movie: { title: pageInfo.title } });
       }
-
       return mediaInfoList;
     },
   };
