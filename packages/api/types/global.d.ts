afc7f82d9a7018927c620867501afbbeb10fd63a
Vitaly Gashkov <vitalygashkov@vk.com>
2024-12-01 13:29:25 +0500

    feat: expose logger to plugin context

--- packages/api/types/global.d.ts
+++ packages/api/types/global.d.ts
@@ -1,9 +1,11 @@
 import { App } from './app';
 import { AppStorage } from './storage';
 import { AppHttp } from './http';
+import { AppLogger } from './logger';
 
 declare global {
   const app: App;
   const storage: AppStorage;
   const http: AppHttp;
+  const logger: AppLogger;
 }
